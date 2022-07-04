64de969fcb614a586fbde1a3f8f81300506a8e5e
gks3075 <97646749+gks3075@users.noreply.github.com>
2022-07-04 21:48:12 +0900

    [Gold IV] Title: 알파벳, Time: 7036 ms, Memory: 150796 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,22 +1,24 @@
-r, c = map(int, input().split())
-maps = []
-for _ in range(r):
-    maps.append(list(input()))
-ans = 0
-alphas = set()
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+# 알파벳
+def dfs(i, j, ans):
+    global max_ans
+    max_ans = max(max_ans, ans)
+    for x in range(4):
+        ni = i + di[x]
+        nj = j + dj[x]
+        if 0 <= ni < R and 0 <= nj < C and not word_array[ni][nj] in alpha:
+            alpha.add(word_array[ni][nj])
+            dfs(ni, nj, ans + 1)
+            alpha.remove(word_array[ni][nj])
 
-def dfs(x, y, count):
-    global ans
-    ans = max(ans, count)
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < r and 0 <= ny < c and not maps[nx][ny] in alphas:
-            alphas.add(maps[nx][ny])
-            dfs(nx, ny, count+1)
-            alphas.remove(maps[nx][ny])
-alphas.add(maps[0][0])
+
+R, C = map(int, input().split())
+word_array = [list(map(str, input())) for _ in range(R)]
+
+max_ans = 0
+di = [-1, 1, 0, 0]
+dj = [0, 0, -1, 1]
+alpha = set()
+alpha.add(word_array[0][0])
 dfs(0, 0, 1)
-print(ans)
+
+print(max_ans)
