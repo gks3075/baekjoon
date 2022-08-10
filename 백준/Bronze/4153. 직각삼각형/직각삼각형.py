c262d54550d2c1fb14e1f9c39aa222f47d14813d
gks3075 <97646749+gks3075@users.noreply.github.com>
2022-08-10 23:35:34 +0900

    [Bronze III] Title: 직각삼각형, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,10 @@
+# 직각삼각형
+while True:
+    array = list(map(int, input().split()))
+    if array[0] == 0 and array[1] == 0 and array[2] == 0:
+        break
+    array.sort()
+    if array[0] ** 2 + array[1] ** 2 == array[2] ** 2:
+        print("right")
+    else:
+        print("wrong")
