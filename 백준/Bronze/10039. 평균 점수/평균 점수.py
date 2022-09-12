66bf5b095a2202291010d0ac77bbf90b7d71205f
gks3075 <97646749+gks3075@users.noreply.github.com>
2022-09-12 23:16:48 +0900

    [Bronze IV] Title: 평균 점수, Time: 104 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+a = a if a >= 40 else 40
+b = b if b >= 40 else 40
+c = c if c >= 40 else 40
+d = d if d >= 40 else 40
+e = e if e >= 40 else 40
+print((a + b + c + d + e) // 5)
