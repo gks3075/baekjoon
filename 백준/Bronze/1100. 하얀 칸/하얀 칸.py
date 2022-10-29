ce21ae53fe0581c53e1b3627e010a5814b8521b4
gks3075 <97646749+gks3075@users.noreply.github.com>
2022-10-29 23:08:38 +0900

    [Bronze II] Title: 하얀 칸, Time: 116 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.py
@@ -0,0 +1,9 @@
+# 하얀 칸
+chess = [list(input()) for _ in range(8)]
+ans = 0
+for i in range(8):
+    for j in range(8):
+        if (i + j) % 2 == 0 and chess[i][j] == 'F':
+            ans += 1
+
+print(ans)
